88d8eeb18be04a06a9381cedcd576de4b98e7786
SANKALP SWAROOP <swaroopsankalp437@gmail.com>
2026-04-01 19:38:11 +0530

    Fix: Add manual CORS headers before cors middleware

--- index.js
+++ index.js
@@ -94,6 +94,18 @@ connectDb();
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://quizaro-frontend.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 app.use(cors({
   origin: ["http://localhost:3000", "https://quizaro-frontend.vercel.app"],
   credentials: true,
